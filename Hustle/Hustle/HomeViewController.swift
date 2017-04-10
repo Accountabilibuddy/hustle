9b3c508ba45aa488a3a1022e4a46ca8f5bcfd3be
EVE DENISON <evedenison@gmail.com>
2017-04-10 10:09:54 -0700

    added mvc structure, new homeview controller, and hv controller storyboard, activated CloudKit

---
+++ Hustle/Hustle/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  Hustle
+//
+//  Created by Eve Denison on 4/10/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+}
