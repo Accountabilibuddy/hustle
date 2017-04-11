4c7432e3b9edaceb1919d57971c73f31fd9c1b5f
EVE DENISON <evedenison@gmail.com>
2017-04-11 10:09:28 -0700

    added hustle controller

---
+++ Hustle/Hustle/HustleController.swift
@@ -0,0 +1,20 @@
+//
+//  HustleController.swift
+//  Hustle
+//
+//  Created by Eve Denison on 4/11/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+import UIKit
+
+class HustleController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
