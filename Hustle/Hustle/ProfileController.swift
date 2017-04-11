68b294d33e33d049f28e6175636b57e0521cfb5b
EVE DENISON <evedenison@gmail.com>
2017-04-11 10:02:50 -0700

    added profie storyboard

---
+++ Hustle/Hustle/ProfileController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileController.swift
+//  Hustle
+//
+//  Created by Eve Denison on 4/11/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+import UIKit
+
+class ProfileController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
