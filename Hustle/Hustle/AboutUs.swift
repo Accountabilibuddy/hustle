e4e2489ec16f96b963159e65e38724d1a53d1537
‘LuayYounus’ <luayghazwan@yahoo.com>
2017-04-13 14:47:40 -0700

    About Us Controller with last changes

---
+++ Hustle/Hustle/AboutUs.swift
@@ -0,0 +1,17 @@
+//
+//  AboutUs.swift
+//  Hustle
+//
+//  Created by Luay Younus on 4/13/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AboutUsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
