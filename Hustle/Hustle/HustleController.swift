0d9c6188f1217d8ddb182d5121b3fad163a74e42
EVE DENISON <evedenison@gmail.com>
2017-04-13 17:19:02 -0700

    styled home page

--- Hustle/Hustle/HustleController.swift
+++ Hustle/Hustle/HustleController.swift
@@ -73,3 +73,38 @@ extension HustleController: UITableViewDataSource, UITableViewDelegate {
         return cell
     }
 }
+
+// Border features by Axel Guilmin
+@IBDesignable extension UIView {
+    @IBInspectable var borderColor:UIColor? {
+        set {
+            layer.borderColor = newValue!.cgColor
+        }
+        
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor:color)
+            }
+            else {
+                return nil
+            }
+        }
+    }
+    @IBInspectable var borderWidth:CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    @IBInspectable var cornerRadius:CGFloat {
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
